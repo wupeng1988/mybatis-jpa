04511c86f1189291f8eb16e1e6e90e4dd22e5112
wupeng <wupeng@yunqiandai.com>
2017-07-25 18:20:35 +0800

    special characters

---
+++ src/main/java/org/apache/ibatis/features/jpa/ddl/SchemaExport.java
@@ -0,0 +1,23 @@
+package org.apache.ibatis.features.jpa.ddl;
+
+/**
+ * Based on entity meta data, check corresponding table meta data
+ *
+ * Created by Adam on 2017/7/25.
+ */
+public class SchemaExport {
+
+    /**
+     *
+     * begin to check db meta data
+     *
+     * @param showScript print the script
+     * @param execute automatically do the ddl operation
+     */
+    public void export(boolean showScript, boolean execute) {
+
+
+
+    }
+
+}
